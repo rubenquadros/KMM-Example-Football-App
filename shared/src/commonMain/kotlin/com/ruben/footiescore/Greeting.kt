c0d5f985b7976ffc4d6eef740b926448cad68a14
rubenquadros <rquadros95@gmail.com>
2021-10-08 23:32:21 +0530

    initial commit

---
+++ shared/src/commonMain/kotlin/com/ruben/footiescore/Greeting.kt
@@ -0,0 +1,7 @@
+package com.ruben.footiescore
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/ruben/footiescore/Platform.kt
@@ -0,0 +1,5 @@
+package com.ruben.footiescore
+
+expect class Platform() {
+    val platform: String
+}
